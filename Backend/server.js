aaa34e3854f93b0eb7e30862a29122d708332b79
sanookdev <supaporn.dev@gmail.com>
2025-11-27 11:30:27 +0700

    Updated origin

--- Backend/server.js
+++ Backend/server.js
@@ -7,14 +7,16 @@ const app = express();
 
 app.use(
   cors({
-    // origin: [
-    //   "http://localhost:5173",
-    //   'http://172.17.1.250:5173',
-    //   "https://med.tu.ac.th",
-    //   "https://med.tu.ac.th/intra_dashboard",
-    //   "https://med.tu.ac.th/intra_dashboard_dev_v1",
-    // ], // frontend
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      'http://172.17.1.250:5173',
+      "https://med.tu.ac.th",
+      "https://med.tu.ac.th/intra_dashboard",
+      "https://med.tu.ac.th/intra_dashboard_dev_v1",
+      "http://203.131.209.137/main/income_display",
+      "http://203.131.209.137"
+    ], // frontend
+    // origin: "*",
     credentials: true, // ถ้ามี cookie/token
   })
 );
